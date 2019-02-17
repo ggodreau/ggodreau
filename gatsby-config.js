1db4c9b15018d11b55535d497a77d83e49d91e76
ggodreau <godreau@gmail.com>
2019-02-17 15:01:27 -0600

    added path prefix to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
+  pathPrefix: "/godreauxyz",
   siteMetadata: {
     title: "godreau.xyz",
     author: "Gregory Godreau",
     description: "godreau.xyz homepage"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#000000',
-        theme_color: '#000000',
-        display: 'minimal-ui',
-        icon: 'src/images/trollface.png', // This path is relative to the root of the site.
-      },
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#000000",
+        display: "minimal-ui",
+        icon: "src/images/trollface.png" // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
-}
+    "gatsby-plugin-sass",
+    "gatsby-plugin-offline"
+  ]
+};
