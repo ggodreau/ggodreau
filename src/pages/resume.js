0f6bcf88c3dd8e47e5981cf127edb493c52fb0a7
ggodreau <godreau@gmail.com>
2019-02-19 15:10:00 -0600

    changed to blue background on resume container div

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,10 +3,8 @@ import CV from "../components/cv";
 import Data from "../data";
 import { Link } from "gatsby";
 
-import Layout from "../components/layout";
-
 const resume = () => (
-  <div>
+  <div className="wrapperwrap">
     <Link to="/" target="_blank" rel="noopener noreferrer">
       Go back to the homepage
     </Link>
